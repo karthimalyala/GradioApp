eb6e1f0e7ff334fa06c2a06fdb9898101dc6577b
Karthi Teja Malyala <84483984+karthimalyala@users.noreply.github.com>
2023-06-13 19:23:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -195,4 +195,4 @@ with gr.Blocks() as demo:
         btn.click(question_answer, inputs=[
                   url, file, question, openAI_key], outputs=[answer])
 
-app = demo.launch(share=True, server_port=8080)
+app = demo.launch(share=True, server_port=8000)
